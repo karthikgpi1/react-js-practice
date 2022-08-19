df85116fc63f4857caf0fbe8e54dd8cbb3dea9f4
karthikgpi1 <karthikgopi024@gmail.com>
2022-08-19 09:05:49 +0530

    JSX part

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//jsx:---------------------------------------
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const element = <p>hello..!</p>;
+const name ="karthik";
+const students = [24,234,345];
+const obj = {name: "gopi"};
+
+
+const ele =<p>hello {students.map ((st) => st)}</p>;
+const ele1 =<p>hello {obj.name}</p>;
+//render the reactapp in the dom:--------------------------
+
+// ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(<p>hello {name}</p>, document.getElementById('root'));
+
+// ReactDOM.render(ele, document.getElementById('root'));
+ReactDOM.render(ele1, document.getElementById('root'));
+
+
