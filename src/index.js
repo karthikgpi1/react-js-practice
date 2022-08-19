c2efbd3c5fadc82ab3356896914c30d1b766d14e
karthikgpi1 <karthikgopi024@gmail.com>
2022-08-19 09:27:43 +0530

    class and functional - components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//jsx:---------------------------------------
+//------------jsx:---------------------------------------
 
 // const element = <p>hello..!</p>;
-const name ="karthik";
-const students = [24,234,345];
-const obj = {name: "gopi"};
+// const name ="karthik";
+// const students = [24,234,345];
+// const obj = {name: "gopi"};
 
 
-const ele =<p>hello {students.map ((st) => st)}</p>;
-const ele1 =<p>hello {obj.name}</p>;
-//render the reactapp in the dom:--------------------------
+// const ele =<p>hello {students.map ((st) => st)}</p>;
+// const ele1 =<p>hello {obj.name}</p>;
+
+//------basic class component----------------
+
+// class App extends React.Component{
+//   render(){
+//     return <p>hello karthik..!</p>;
+//   }
+// }
+//----function component-----
+ function App(){
+  return <p>hello karthik...!</p>;
+ }
+
+//----------render the reactapp in the dom:--------------------------
 
 // ReactDOM.render(element, document.getElementById('root'));
 // ReactDOM.render(<p>hello {name}</p>, document.getElementById('root'));
 
 // ReactDOM.render(ele, document.getElementById('root'));
-ReactDOM.render(ele1, document.getElementById('root'));
+// ReactDOM.render(ele1, document.getElementById('root'));
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));                //<App/>
+                                                                         //const app = new app();
+                                                                         //app.render();
 
 
